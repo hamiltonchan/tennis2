c8fd5f7dab72d1ebe0f27ec40ce2ea2275a2f1d4
Hamilton Chan <hamiltonchanjr@gmail.com>
2014-02-03 07:00:18 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tennis2app::Application.config.secret_key_base = 'a469e6fe89f102d601bb08208f1fff871ea23ac0ca21304b91436748bae5f2922de2d10f9c60c0c43f6da3fa5d1716ae0ef2730a3da31374d1a0589f18bca71d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tennis2app::Application.config.session_store :cookie_store, key: '_tennis2app_session'
